01de70320c5925f31bfe37c64cfc45cbc6a9cf6b
ItsAqss <k20170@nu.edu.pk>
2024-01-31 19:40:48 +0500

    token removed

--- app/routes/ad.routes.js
+++ app/routes/ad.routes.js
@@ -5,7 +5,7 @@ const ad_route = express.Router();
 const { handleImageUpload } = require('../controllers/app.controller');
 const {upload} = require('../utils/imageUpload');
 
-ad_route.post('/uploadImage',[verifyToken, upload.single('image')], handleImageUpload);
+ad_route.post('/uploadImage',[ upload.single('image')], handleImageUpload);
 ad_route.post('/postAd', [verifyToken], ad.postAd);
 ad_route.get('/getAllAds', [verifyToken], ad.getAllAds);
 module.exports = ad_route;
