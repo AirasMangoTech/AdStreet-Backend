8bb2fb85499a525c7e98221714a5eb26b343c6a1
ItsAqss <k20170@nu.edu.pk>
2024-03-02 12:29:02 +0500

    Fix typo in ROLE_IDS constant

--- app/utils/utility.js
+++ app/utils/utility.js
@@ -1,7 +1,7 @@
 const ROLE_IDS = {
   BRAND_COMPANY: "Service Seeker", // service seeker
   AGENCY: "Service Provider", // service provider
-  INDIVIDUAL: "INDIVIDUAL", // indivisual
+  INDIVIDUAL: "Indivisual", // indivisual
   ADMIN: "ADMIN"
 };
 
