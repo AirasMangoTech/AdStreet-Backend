8d6853a20aa76ed547960ea365bd40bd89dfe434
Aqsa Ahmed <k20170@nu.edu.pk>
2024-05-01 11:43:02 +0500

    Add getAllWEB route to banner.routes.js

--- app/routes/banner.routes.js
+++ app/routes/banner.routes.js
@@ -6,6 +6,7 @@ const banner_route = express.Router();
 // Create a new Banner
 banner_route.post("/create", [verifyToken], banner.createBanner);
 banner_route.get("/getAll", [verifyToken], banner.getAllBanners);
+banner_route.get("/getAllWEB",  banner.getAllBanners);
 banner_route.put("/update/:id", [verifyToken], banner.updateBanner);
 banner_route.delete("/delete/:id", [verifyToken], banner.deleteBanner);
 
