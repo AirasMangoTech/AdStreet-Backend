ecb2fa3a2aed9a794b51c907ccd435901cc07ab2
Aqsa Ahmed <k20170@nu.edu.pk>
2024-05-08 18:53:42 +0500

    Refactor getMessages function in chat.controller.js

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -1,28 +1,14 @@
 const Chat = require("../models/chats");
+const response = require("../utils/responseHelpers");
 
+// get all messages from chat id and user id
 const getMessages = async (req, res) => {
   try {
-    // Extract chatId and userId from request parameters
-    const { id, userId } = req.body;
-
-    // Validate chatId and userId
-    if (!id || !userId) {
-      return res.status(400).json({ error: 'ChatId and userId are required' });
-    }
-
-    // Query messages based on chatId and userId
-    const messages = await Chat.find({ id, userId });
-
-    // Check if messages are found
-    if (messages.length === 0) {
-      return res.status(404).json({ error: 'No messages found' });
-    }
-
-    // Return the messages
-    return res.status(200).json({ messages });
+    const { chatId, userId } = req.body;
+    
+    return response.success(res, "chatId and UserID loaded successfully", { chatId, userId });
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Internal server error' });
+    return response.serverError(res, error.message, "Failed to load Messages");
   }
 };
 
