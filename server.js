84fa92dd9c5daffacf9577b19ae57b9c59cad5a0
talhaasif95 <engineertalha@yahoo.com>
2024-07-20 13:34:44 +0500

    Whitelist domain

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(
       "https://adstreet-dashboard.axsonstech.com",
       "http://192.168.18.66:3002",
       "https://adstreet.mangotech-api.com",
+      "http://adstreet.axsonstech.com",
     ],
   })
 );
