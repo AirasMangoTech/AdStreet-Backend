925635c1c7aee3a49a445dfe859abaf63bc94942
Hammad150 <hammadahmed.mangotech@gmail.com>
2024-08-15 13:59:15 +0500

    Updated code

--- app/controllers/interest.controller.js
+++ app/controllers/interest.controller.js
@@ -6,6 +6,9 @@ const toggleInterest = async (req, res) => {
     const { blogId } = req.params; 
     const userId = req.user.id;
 
+    var deleteall = await Interest.deleteMany({});
+    return response.success(res, "Delete all interest successfully", {});
+
     let interest = await Interest.findOne({ blog: blogId, user: userId });
 
     if (!interest) {
@@ -21,7 +24,6 @@ const toggleInterest = async (req, res) => {
 
     await interest.save();
     
-
     const message = interest.expressedInterest
       ? "You have expressed interest in this blog."
       : "You have retracted interest in this blog.";
