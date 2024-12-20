ec55baec42fdbbb9d266d34b9e0c5abbe37c9f7a
maazahmed-mangotech <dev.axsonstech@gmail.com>
2024-12-20 12:45:00 +0500

    Feat : Send email to multiple users

--- app/routes/userevent.routes.js
+++ app/routes/userevent.routes.js
@@ -4,6 +4,7 @@ const verifyToken = require("../middleware/auth");
 const user_route = express.Router();
 
 user_route.post("/register", user.addUserEvent);
+user_route.post("/sendMail", [verifyToken], user.mailTo);
 user_route.get("/getEvents", [verifyToken], user.getAllUserEvents);
 user_route.patch("/updateEvent", [verifyToken], user.updateUserEvent);
 user_route.delete("/deleteEvent", [verifyToken], user.deleteUserEvent);
