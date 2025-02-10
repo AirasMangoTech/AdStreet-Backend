dde8da91f039c40c36624de0fbbc662fae6d654b
maazahmed-mangotech <dev.axsonstech@gmail.com>
2025-02-10 12:01:52 +0500

    fix: Firebase notifications

--- app/utils/sendNotifications.js
+++ app/utils/sendNotifications.js
@@ -12,7 +12,6 @@ const sendNotification = async (title, body, data, token) => {
         title: title,
         body: body,
       },
-      token: token,
       data: { obj: JSON.stringify(data) },
     };
 
@@ -30,7 +29,7 @@ const sendNotification = async (title, body, data, token) => {
     //   });
 
     // For Multiple Notification
-    const response = await firebase.messaging().send(payload);
+    const response = await firebase.messaging().sendToDevice(token, payload)
     console.log("Firebase Response:", response);
 
     console.log("im running");
